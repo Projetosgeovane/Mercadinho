860456c828d3792e91efc7f5434bfb881c31e5d6
Geovane Silva <geovanesilva.nar@hotmail.com>
2021-09-23 21:42:51 -0300

    Domain e Contract

---
+++ src/Mercadinho.Prateleira.Domain/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercadinho.Prateleira.Domain
+{
+    class Produto
+    {
+        public int id { get; set; }
+        public string Descricao { get; set; }
+        public ICollection<Categoria> Categorias { get; set; }
+        public Estoque Estoque { get; set; } = default;
+    }
+}
